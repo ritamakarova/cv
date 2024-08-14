7ace7e6eb95a8bd8293dbe5b210905da719d8408
Маргарита <ritamakarova@yandex.ru>
2024-08-14 18:20:36 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function () {
+	alert('https://www.ebay.com/itm/204626055388?itmmeta=01J56P1XC1XYJTE33MCT6Q866Q&hash=item2fa4a9d0dc:g:~6QAAOSwfLFlreOz')
+
+	
+})
